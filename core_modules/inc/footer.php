807b25faf3cae329e4a3ff7b3fa0bf6753a17c38
chris <chris@ckvsoft.at>
2025-10-04 14:07:46 +0200

    fix: add missing framework name
    Signed-off-by: chris <chris@ckvsoft.at>

--- core_modules/inc/footer.php
+++ core_modules/inc/footer.php
@@ -1,6 +1,6 @@
 </div>
 <div class="fixed-footer">
-    <div class="container">Copyright &copy; 1998 - <?= date("Y"); ?> Christian Kvasny (ckvsoft.at) Version <?= \ckvsoft\Version::version() ?></div>
+    <div class="container">cevian - Copyright &copy; 1998 - <?= date("Y"); ?> Christian Kvasny (ckvsoft.at) Version <?= \ckvsoft\Version::version() ?></div>
 </div>
 <div id="statusmessage"></div>
 </body>
