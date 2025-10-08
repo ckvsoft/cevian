324cdbca54b2a167e1b0add626c72ffd0a3d5630
chris <chris@ckvsoft.at>
2025-10-08 03:39:09 +0200

    bumpup
    Signed-off-by: chris <chris@ckvsoft.at>

--- library/ckvsoft/version.php
+++ library/ckvsoft/version.php
@@ -9,7 +9,7 @@ class Version
      * Base version of the application (without Git info)
      * @var string
      */
-    private static $baseVersion = "0.8.3-251008";
+    private static $baseVersion = "0.9.0-251008";
 
     /**
      * Returns the application name
