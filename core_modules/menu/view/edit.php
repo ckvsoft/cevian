dd3fecdce0289c1bdb66dfd3a0039190a29126f9
chris <chris@ckvsoft.at>
2025-10-16 22:05:11 +0200

    refactor(menu/view): Apply internationalization to menu edit form
    This commit refactors the menu edit view (`edit.php`) to prepare it for multilingual support by wrapping all user-facing static text (labels, button values, and select options) with the `_()` translation function.
    
    Key Changes:
    1.  **I18n Integration:** All labels ('Menuentry', 'Edit', 'ID', 'Label', 'Link', 'Parent', 'Sort', 'Role', 'None', 'Admin', 'Owner', 'Public') and button texts ('Save', 'Cancel') are now translated.
    2.  **ID Display:** The read-only `ID` field is explicitly added to the form for better clarity.
    
    Signed-off-by: chris <chris@ckvsoft.at>

--- core_modules/menu/view/edit.php
+++ core_modules/menu/view/edit.php
@@ -1,29 +1,39 @@
 <fieldset>
-    <legend>Menuentry: Edit</legend>
+    <legend><?php echo _('Menuentry') . ': ' . _('Edit'); ?></legend>
     <div data-form="editForm">
         <form action="<?php echo BASE_URI; ?>menu/editSave/<?php echo $this->menuList[0]['id']; ?>" method="post" id="editForm" data-redirect="menu">
-            <label for="label">Label:</label>
+
+            <label for="id">ID:</label>
+            <input type="text" id="id" value="<?php echo $this->menuList[0]['id']; ?>" readonly><br />
+            <label for="label"><?php echo _('Label'); ?>:</label>
             <input type="text" id="label" name="label" value="<?php echo $this->menuList[0]['label']; ?>" required><br />
-            <label for="link">Link:</label>
+
+            <label for="link"><?php echo _('Link'); ?>:</label>
             <input type="text" id="link" name="link" value="<?php echo $this->menuList[0]['link']; ?>" required><br />
-            <label for="parent">Parent:</label>
+
+            <label for="parent"><?php echo _('Parent'); ?>:</label>
             <input type="text" id="parent" name="parent" value="<?php echo $this->menuList[0]['parent']; ?>"><br />
-            <label for="sort">Sort:</label>
+
+            <label for="sort"><?php echo _('Sort'); ?>:</label>
             <input type="text" id="sort" name="sort" value="<?php echo $this->menuList[0]['sort']; ?>"><br />
+
             <div id="right">
-                <label for="role">Role:</label>
+                <label for="role"><?php echo _('Role'); ?>:</label>
                 <select name="role">
-                    <option value="None" <?php if ($this->menuList[0]['role'] == 'None') echo 'selected'; ?>>None</option>
-                    <option value="admin" <?php if ($this->menuList[0]['role'] == 'admin') echo 'selected'; ?>>Admin</option>
-                    <option value="owner" <?php if ($this->menuList[0]['role'] == 'owner') echo 'selected'; ?>>Owner</option>
+                    <option value="None" <?php if ($this->menuList[0]['role'] == 'None') echo 'selected'; ?>><?php echo _('None'); ?></option>
+                    <option value="admin" <?php if ($this->menuList[0]['role'] == 'admin') echo 'selected'; ?>><?php echo _('Admin'); ?></option>
+                    <option value="owner" <?php if ($this->menuList[0]['role'] == 'owner') echo 'selected'; ?>><?php echo _('Owner'); ?></option>
                 </select><br /><br />
             </div><br />
-            <label for="is_public">Public:</label>
+
+            <label for="is_public"><?php echo _('Public'); ?>:</label>
             <input type="hidden" name="is_public" value="-1">
             <input class="checkbox" type="checkbox" name="is_public" value="1" <?php if ($this->menuList[0]['is_public'] == '1') echo 'checked'; ?>><br />
+
             <br /><br />
-            <input class="button small-action save" type="submit">
-            <input class="button small-action cancel" type="button" onclick="javascript:window.location = '<?php echo BASE_URI; ?>menu';" value="Cancel">
+
+            <input class="button small-action save" type="submit" value="<?php echo _('Save'); ?>">
+            <input class="button small-action cancel" type="button" onclick="javascript:window.location = '<?php echo BASE_URI; ?>menu';" value="<?php echo _('Cancel'); ?>">
         </form>
     </div>
 </fieldset>
