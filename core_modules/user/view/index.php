93705f8dedf70852cce225d962f8b8f029f1a56a
chris <chris@ckvsoft.at>
2025-10-23 15:11:53 +0200

    refactor(user/view): Internationalize user creation form (i18n)
    This commit updates the user creation form and user list section in the `index.php` view to support multiple languages by wrapping all user-facing strings with the Gettext translation function `_()`.
    
    Key Changes:
    
    1.  **Fieldset Legends:** Internationalizes titles for both the "Add User" and "Existing Users" sections.
    2.  **Form Labels:** Internationalizes all form labels: 'Name', 'Email', 'Password', and 'Role'.
    3.  **Role Options:** Internationalizes the fixed role options: 'Owner' and 'Admin'.
    4.  **Buttons:** Internationalizes the submit button ('Create User') and the reset button ('Clear').
    5.  **Security/UX:** Corrects the `autocomplete` attribute for the password field to **`new-password`** to assist password managers during creation.
    
    Signed-off-by: chris <chris@ckvsoft.at>

--- core_modules/user/view/index.php
+++ core_modules/user/view/index.php
@@ -1,31 +1,31 @@
 <fieldset>
-    <legend>User: Add</legend>
+    <legend><?= _('User: Add') ?></legend>
     <div id="addUser" class="ajax-form-container" data-form="userForm" data-url="user/userList">
         <form action="user/create" method="post" id="userForm" autocomplete="off">
-            <label for="username">Name:</label>
+            <label for="username"><?= _('Name') ?>:</label>
             <input type="text" id="username" name="username" required autocomplete="off"><br />
 
-            <label for="email">Email:</label>
+            <label for="email"><?= _('Email') ?>:</label>
             <input type="text" id="email" name="email" required autocomplete="off"><br />
 
-            <label for="password">Password:</label>
+            <label for="password"><?= _('Password') ?>:</label>
             <input type="password" id="password" name="password" required autocomplete="new-password"><br />
 
-            <label for="role">Role:</label>
+            <label for="role"><?= _('Role') ?>:</label>
             <select name="role">
-                <option value="owner">Owner</option>
-                <option value="admin">Admin</option>
+                <option value="owner"><?= _('Owner') ?></option>
+                <option value="admin"><?= _('Admin') ?></option>
             </select>
 
             <br /><br />
 
-            <input class="button small-action save" type="submit" value="Create User">
+            <input class="button small-action save" type="submit" value="<?= _('Create User') ?>">
             <input class="button small-action cancel" type="reset" value="<?= _('Clear') ?>">
 
         </form>
     </div>
 </fieldset>
 <fieldset style="margin-top: 30px;">
-    <legend>Existing User</legend>
+    <legend><?= _('Existing Users') ?></legend>
     <div id="userlist" class="ajax-list" data-list="user/userList"></div>
 </fieldset>
