d78db9c330ae52369bfc89bddf913581a1a1ae69
chris <chris@ckvsoft.at>
2025-10-23 14:44:39 +0200

    feat(gallery/view): Display extended media metadata in detail view
    This commit updates the 'media.php' template to display the newly available file metadata: file size and modification date.
    
    Key Changes:
    
    1.  **Copyright Header:** Adds the standard MIT License copyright header for consistency.
    2.  **Display Size:** Adds the `Size` field (`$item['size']`) to the media statistics panel.
    3.  **Display Date:** Adds the `Date` field (`$item['date_formatted']`) to the media statistics panel, providing the file's modification time.
    
    These additions enhance the information available to the user when viewing a specific media item.
    
    Signed-off-by: chris <chris@ckvsoft.at>

--- core_modules/gallery/view/manager/media.php
+++ core_modules/gallery/view/manager/media.php
@@ -1,7 +1,28 @@
 <?php
-/** * @var array $album ...
- * @var array $media ...
+/*
+ * The MIT License
+ *
+ * Copyright 2025 chris.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 $baseUri = BASE_URI;
 $album = $this->album;
 $media = $this->media;
@@ -50,8 +71,10 @@ $media = $this->media;
                     </p>
 
                     <p class="media-stats">
+                        Size: <?= $item['size'] ?><br />
                         Views: <?= number_format($item['views'] ?? 0) ?><br>
-                        Last View: <?= htmlspecialchars($item['last_view'] ?? '-') ?>
+                        Last View: <?= htmlspecialchars($item['last_view'] ?? '-') ?><br />
+                        Date: <?= $item['date_formatted'] ?>
                     </p>
 
                     <div class="media-actions">
